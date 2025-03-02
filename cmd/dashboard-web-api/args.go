dcbf1904c421d39a788fb8eb457afb14463edab9
Kensuke Yanai <y2aiskni@gmail.com>
2025-03-02 15:00:13 +0900

    feat: バージョン出力を実装

--- cmd/dashboard-web-api/args.go
+++ cmd/dashboard-web-api/args.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/alecthomas/kong"
+
 var args struct {
+	Version kong.VersionFlag `name:"version" short:"v" help:"Show version information."`
 }
--- cmd/dashboard-web-api/main.go
+++ cmd/dashboard-web-api/main.go
@@ -6,8 +6,10 @@ import (
 	"github.com/alecthomas/kong"
 )
 
+var version = "0.0.0"
+
 func main() {
-	ctx := kong.Parse(&args)
+	ctx := kong.Parse(&args, &kong.Vars{"version": version})
 	switch ctx.Command() {
 	case "":
 		fmt.Println("execute!")
